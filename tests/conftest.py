7a44f7d77552cbfa19c5e4da8e29bc4b9bbb7dae
Drino <drinob@gmail.com>
2025-08-22 15:13:21 +0300

    Added init_flow_runner_dirs fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,18 @@ import pytest
 
 import yt.wrapper as yt
 
+from src.cli.commands import _ensure_table
+from src.dag_entity import DagEntityRow
+from src.dag_run import DagRunRow
+from src.task_run import TaskRunRow
+
+
+@pytest.fixture(autouse=True, scope="session")
+def init_flow_runner_dirs(yt_client: yt.YtClient):
+    for row_type in [DagEntityRow, DagRunRow, TaskRunRow]:
+        _ensure_table(yt_client, row_type)
+
+
 def pytest_addoption(parser):
     parser.addoption("--proxy", action="store", default="localhost:8000")
 
